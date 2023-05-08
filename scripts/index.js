fc16af1214aa1862d78e8539d26ba0ae8186ac92
JohnZabelski <johnzabelski@yandex.ru>
2023-05-09 01:15:22 +0300

    mesto правки

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,18 @@
 const popupElement = document.querySelector('.popup');
-const profilepopupElement = document.querySelector('.profile__button');
+const profilePopupElement = document.querySelector('.profile__button');
 const popupcloseElement = document.querySelector('.popup__close-button');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.popup__form');
-const nameInput = document.querySelector('.popup__form-input-title');
-const jobInput = document.querySelector('.popup__form-input-subtitle');
-
-const togglePopupVisibility = function () {
-    popupElement.classList.add('popup__opened');
+const nameInput = document.querySelector('.popup__form-input_text_title');
+const jobInput = document.querySelector('.popup__form-input_text_subtitle');
+const openPopup = function () {
+    popupElement.classList.add('popup_opened');
 }
-const togglePopupUnVisibility = function () {
-    popupElement.classList.remove('popup__opened');
+const closePopup = function () {
+    popupElement.classList.remove('popup_opened');
 }
 
-profilepopupElement.addEventListener('click', togglePopupVisibility);
-popupcloseElement.addEventListener('click', togglePopupUnVisibility);
 
 function setPopupInputValue() {
     nameInput.value = profileTitle.textContent;
@@ -27,16 +24,20 @@ function setTextValue() {
     profileSubtitle.textContent = jobInput.value;
 }
 
-profilepopupElement.addEventListener('click', function ()  {
-            setPopupInputValue();
-            togglePopupVisibility(popupElement);
-    });
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
     setTextValue();
-    togglePopupUnVisibility(popupElement);
+    closePopup();
 }
 
+
+profilePopupElement.addEventListener('click', openPopup);
+popupcloseElement.addEventListener('click', closePopup);
+profilePopupElement.addEventListener('click', function ()  {
+            openPopup(popupElement);
+    });
+profilePopupElement.addEventListener('click', function ()  {
+            setPopupInputValue();
+});
 
 formElement.addEventListener('submit', handleFormSubmit); 
